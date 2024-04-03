0bb9c451fa265946d96df5b589d15ca6a76533be
Jürgen Gorobez <university@gorobez.com>
2024-04-03 09:36:02 +0200

    trying flatdict out

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 import json
 import numpy as np
 import pandas as pd
+import flatdict as fd
 
 with open('C:\\Users\\NiceRice\\git\\scraper_ochama\\scraper_ochama\\ochama_structure.txt') as f:
     data: list = json.load(f)
+
+
+
 
 # generate parent id list
 df_parent_id: list = []
@@ -12,13 +16,16 @@ for i in data:
     id_parent = i["parentId"]
     df_parent_id.append(id_parent)
 
+print(enumerate(df_parent_id))
 # organize list
-all_in: dict = {}
-for i in data:
+all_in: list = []
+for cont, i in enumerate(data):
     name = i["name"]
     id_parent = i["parentId"]
     id_child = i["id"]
-    all_in["id_parent"] = {"id_parent": id_parent, "id_child": id_child, "name": name}
-
+    print(cont, i)
+    #print(df_parent_id)
+   # all_in[df_parent_id] = [id_parent, id_child, name]
+    #df_parent_id += 1
 print(len(df_parent_id))
 
