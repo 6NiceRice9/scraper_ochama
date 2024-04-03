2527ff93db3b795cb4cf78151da3750e8f38f6df
Jürgen Gorobez <university@gorobez.com>
2024-04-03 20:34:44 +0200

    corrected by chatgpt4

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
-import requests
 import json
-import numpy as np
 import pandas as pd
-import flatdict as fd
-import itertools
 
-# %%% load tree for POST request
-with open('C:\\Users\\NiceRice\\git\\scraper_ochama\\scraper_ochama\\ochama_structure.txt') as f:
-    all_in: list = json.load(f)  # convert
-    all_in: dict = all_in[:]  # convert
+# Load the tree structure from a JSON file.
+tree_path = 'ochama_structure.txt'  # Define the file path.
+with open(tree_path, 'r') as f:
+    tree_data = pd.json_normalize(json.load(f))  # Directly normalize the JSON to a pandas DataFrame.
 
-# %%% organise structure
-# flatten
-all: pd.DataFrame = pd.json_normalize(all_in)
-# get parents overview
-parents_overview: pd.DataFrame = all.loc[all['parentId'] == 0]
-# child overview
-child_overview = all[all['parentId'].isin(parents_overview['id'])]
-# group_overview
-group_overview = all[all['parentId'].isin(child_overview['id'])]
+# Organize structure
+# Identify the parent and child relationships.
+parents_overview = tree_data[tree_data['parentId'] == 0]
+child_overview = tree_data[tree_data['parentId'].isin(parents_overview['id'])]
+group_overview = tree_data[tree_data['parentId'].isin(child_overview['id'])]
 
-# %%% select group
-nested_all = {}
-for i in all["level"].unique():
-    nested_all[i] = all[all["level"] == i]
+# Categorize all items by their level.
+nested_all = {level: group for level, group in tree_data.groupby('level')}
 
-
-# %% link all groups together
-# group = 4808  ## variable
-
-
-def link_tree(group: int) -> tuple:
+def link_tree(group_id):
+    """Function to link a group to its parent and child, returning a tuple of their IDs."""
     try:
-        child: pd.core.series.Series = nested_all[3].loc[nested_all[3]['id'] == group]["parentId"]  # get child
-        child: int = int(child.values[0])  # convert from pd series to int
-        parent = nested_all[2].loc[nested_all[2]['id'] == child]["parentId"]
-        parent: int = int(parent.values[0])  # convert from pd series to int
-        grouped = (parent, child, group)
-    except:
-        grouped = (None, None, group)  # some groups have no children/parents
-    return grouped
+        # Attempt to find the child and parent for the specified group.
+        child_id = int(nested_all[3].loc[nested_all[3]['id'] == group_id, 'parentId'].values[0])
+        parent_id = int(nested_all[2].loc[nested_all[2]['id'] == child_id, 'parentId'].values[0])
+        return parent_id, child_id, group_id
+    except IndexError:  # Handle cases where the group does not have a parent or child.
+        return None, None, group_id
 
+#%%
+# Apply the function to all groups in level 3.
+group_ids = nested_all[3]['id'].values
+groups_connected = {group_id: link_tree(group_id) for group_id in group_ids}
 
-get_groups = nested_all[3]["id"].values[:]
-# print(len(get_groups))
-# %%
-# print(get_groups[:])
-groups_connected = {}
-i = 1
-# %%
-for i in range(1, len(get_groups)):
-    # print(i, get_groups[i])
-    try:
-        groups_connected[int(get_groups[i])] = link_tree(get_groups[i])
-    except:
-        print("error")
-    # groups_connected[get_groups[i]] = link_tree(get_groups[i])
-# print(groups_connected)
+# Optionally, print or further process `groups_connected` as needed.
+#%%
