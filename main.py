0b645f2fd9942a31e97f93f0534a1ff8a3688f2c
Jürgen Gorobez <university@gorobez.com>
2024-04-03 18:06:17 +0200

    "group"  value has to change and child and parent will be linked

--- main.py
+++ main.py
@@ -6,86 +6,32 @@ import flatdict as fd
 import itertools
 
 
-# load tree for POST request
+#%%% load tree for POST request
 with open('C:\\Users\\NiceRice\\git\\scraper_ochama\\scraper_ochama\\ochama_structure.txt') as f:
     all_in: list = json.load(f)       # convert
     all_in: dict = all_in[:]          # convert
 
-#### organise structure
+#%%% organise structure
 # flatten
 all: pd.DataFrame = pd.json_normalize(all_in)
 # get parents overview
 parents_overview: pd.DataFrame = all.loc[all['parentId'] == 0]
 # child overview
 child_overview = all[all['parentId'].isin(parents_overview['id'])]
-# sub_child_sorting_overview
-sub_child_sorting_overview = all[all['parentId'].isin(child_overview['id'])]
+# group_overview
+group_overview = all[all['parentId'].isin(child_overview['id'])]
 
-
-
-
-
-#%%
-for i in parents_id.loc[:, "id"]:   #parents loop
-    for j in all[all['parentId'] == i]:       #child loop
-        trash.append(all[all['id'] == j])
-        print(i)
-#matches = all[all['parentId'] == 4722]
+#%%% select group
+nested_all ={}
+for i in all["level"].unique():
+    nested_all[i] = all[all["level"] == i]
 
 #%%
-import pandas as pd
+group = 4808    ## variable
+child: pd.core.series.Series = nested_all[3].loc[nested_all[3]['id'] == group]["parentId"] # get child
+child: int = int(child.values[0]) # convert from pd series to int
+parent = nested_all[2].loc[nested_all[2]['id'] == child]["parentId"]
+parent: int = int(parent.values[0]) # convert from pd series to int
+print(parent)
 
-# Sample data for df1
-data1 = {
-    'ID': [1, 2, 3, 4, 5],
-    'Value': ['A', 'B', 'C', 'D', 'E']
-}
-df1 = pd.DataFrame(data1)
-
-# Sample data for df2
-data2 = {
-    'ID': [2, 4]
-}
-df2 = pd.DataFrame(data2)
-
-# Selecting rows from df1 where IDs match those in df2
-matched_df = df1[df1['ID'].isin(df2['ID'])]
-
-print(matched_df)
-
-
-#%%
-for i in parents_id.loc[:, "id"]:   #parents loop
-    for j in all.loc[:, "id"]:       #child loop
-        trash.append(i)
-
-
-
-#%% working section
-# generate parent id list
-df_parent_id: list = []
-for i in post_data:
-    id_parent = i["parentId"]
-    df_parent_id.append(id_parent)
-
-
-
-
-
-
-#pandas df
-df_pandas = pd.DataFrame()
-
-
-
-# organize list
-all_in: list = []
-for cont, i in enumerate(post_data):
-    name = i["name"]
-    id_parent = i["parentId"]
-    id_child = i["id"]
-
-
-
-print(len(df_parent_id))
 
