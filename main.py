d996097353eda1e237bd4371a9f31bd1f6aca9bc
Jürgen Gorobez <university@gorobez.com>
2024-04-04 03:14:35 +0200

    all parameter in one dict

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 import pandas as pd
 
 
-# # Organize structure
-# # Identify the parent and child relationships.
-# parents_overview = tree_data[tree_data['parentId'] == 0]
-# child_overview = tree_data[tree_data['parentId'].isin(parents_overview['id'])]
-# group_overview = tree_data[tree_data['parentId'].isin(child_overview['id'])]
-
 # Categorize all items by their level.
 def group_by_level(tree_data: pd.DataFrame, level: int) -> dict:
+    """
+    Function to categorize all items by their level.
+    :param tree_data: raw loaded file input
+    :param level: group level (1, 2 or 3)
+    :return: nested dictionary
+    """
     nested_all: dict = {level: group for level, group in tree_data.groupby('level')}
     return nested_all
 
@@ -33,14 +33,14 @@ def all_group_ids(nested_all: dict) -> dict:
 
 
 # %% getting the valeus by id
-def all_values_by_id(nested_dict: dict, id: int, header: str):
+def all_values_by_id(id: int, header: str):
     """
     Function to get the values of a column by ID.
-    :param nested_dict: three level nested template structure from ochama homepage
     :param id: group_id from the deepest level
     :param header: column header
     :return header value at given id
     """
+    public = nested_all
     value = None  # Initialize value as None to indicate "not found" by default
     for i in range(1, 4):
         df = nested_all.get(i)  # Safely get the DataFrame for key `i`
@@ -54,8 +54,6 @@ def all_values_by_id(nested_dict: dict, id: int, header: str):
     return value
 
 
-
-
 ################# Start of the main program #################
 # Load the tree structure from a JSON file.
 tree_path: str = 'ochama_structure.txt'  # Define the file path.
@@ -64,15 +62,32 @@ with open(tree_path, 'r') as f:
 
 # create a dictionary of all groups
 nested_all: dict = group_by_level(tree_data, 3)  # asumption total of 3 levels deep
-# separate 3 levels
-level1 = nested_all[1]
-level2 = nested_all[2]
-level3 = nested_all[3]
+
 # create connceted groups
 groups_connected: dict = all_group_ids(nested_all)
 
-
 # %% make dictionary
 
-print(all_values_by_id(nested_all, 4777, "name"))
-print(all_values_by_id(nested_all, 4777, "imageUrl"))
+print(all_values_by_id(4808, "name"))
+print(all_values_by_id(4808, "imageUrl"))
+
+# %%% preparing request
+overview = []
+group_ids: list = nested_all[3]['id'].values
+for i in range(1, 2):
+    group_id = group_ids[i]
+    child_id = all_values_by_id(group_id, "parentId")
+    parent_id = all_values_by_id(child_id, "id")
+
+    overview.append(
+        {"parent_id:": all_values_by_id(parent_id, "id"),
+         "child_id:": all_values_by_id(child_id, "id"),
+         "group_id:": all_values_by_id(group_id, "id"),
+         "parent_name": all_values_by_id(group_ids[i], "name"),
+         "child_name": all_values_by_id(group_ids[i], "name"),
+         "group_name": all_values_by_id(group_ids[i], "name"),
+         "url": all_values_by_id(group_ids[i], "imageUrl")
+         })
+
+print(overview)
+# print(all_values_by_id(groups_connected[4808][1], "name"))
