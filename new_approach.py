793ba86f8a2adaef102471d786f85ab13f488c70
Jürgen Gorobez <university@gorobez.com>
2024-04-05 10:07:21 +0200

    working

--- new_approach.py
+++ new_approach.py
@@ -4,6 +4,8 @@ import requests  # for sending POST requests
 import numpy as np
 import time
 import random
+from openpyxl import load_workbook
+#%%
 
 tree_path: str = 'ochama_structure.txt'
 with open(tree_path, 'r') as f:
@@ -32,15 +34,15 @@ parents, children, groups, group_without_parent = split_in_groups(raw_json)
 
 #### searching by: parent -> children -> groups
 looking_for = "Fresh"
-looking_for_id_parent = parents[parents["name"] == "Fresh"]  # parent row
+looking_for_id_parent = parents[parents["name"] == "Fresh"].reset_index()  # parent row
 looking_for_children = children[
-    children["parentId"].isin(looking_for_id_parent["id"])]  # matching children to parent id
-looking_for_groups = groups[groups["parentId"].isin(looking_for_children["id"])]  # match groups to children and parent
+    children["parentId"].isin(looking_for_id_parent["id"])].reset_index()  # matching children to parent id
+looking_for_groups = groups[groups["parentId"].isin(looking_for_children["id"])].reset_index()  # match groups to children and parent
 
 # %%
 # constructing tree
 
-def header_request(id_group, page=1, pageSize=1000, sortType="sort_dredisprice_asc"):
+def header_request(id_group: int, page=1, pageSize=1000, sortType="sort_dredisprice_asc"):
     """
     :param id_group:
     :param page:
@@ -62,9 +64,26 @@ def header_request(id_group, page=1, pageSize=1000, sortType="sort_dredisprice_a
 
     request = requests.post("https://www.ochama.com/api/v1/category/aggregate/sku", headers=headers,
                             json=data)  # the right way to send POST requests
-    return request.json()
+    return pd.DataFrame(request.json()["content"])
 
-test_request = {}
+all_requests = pd.DataFrame()
+for i in range(30, len(looking_for_groups["id"])):
+    group_id = int(looking_for_groups["id"].values[i])
+    all_requests = pd.concat([all_requests, header_request(group_id)], ignore_index=True)
+    ##delay
+    delay = random.uniform(2, 5)
+    print(f"Waiting {delay:.2f} seconds...")
+    time.sleep(5)
+
+
+#%%
+
+file_path = "C:/Users/NiceRice/git/scraper_ochama/scraper_ochama/ochama_products.xlsx"
+sheet_name = looking_for
+all_requests.to_csv(f"{looking_for}.txt", index=False)
+
+
+#%%
 group_id = 5099
 test_request = {group_id: header_request(group_id)}
 
